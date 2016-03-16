ebcef9a805f2023ba7c9f3bd5fce37b86656da96
awk11 <awk9293@ad.rit.edu>
2016-03-16 14:28:01 -0400

    last changed file
    should break. but why?

--- src/router.js
+++ src/router.js
@@ -31,6 +31,10 @@ var router = function(app) { //pass the express app in
     
     //When someone POSTS to /updateLast, call controllers.updateLast
     app.post('/updateLast', controllers.updateLast);
+	
+	app.post('/setDogName', controllers.setDogName);
+	app.post('/updateLastDog', controllers.updateLastDog);
+	app.get('/findDogByName', controllers.searchDogName);
 };
 
 //export the router function
